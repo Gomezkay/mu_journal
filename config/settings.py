1daa9ff27f2c17fe448cabd8c39e0afe2268589b
Gomezkay <46538878+Gomezkay@users.noreply.github.com>
2026-04-22 06:49:23 +0200

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,7 @@ TIME_ZONE = "Africa/Lusaka"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 MEDIA_URL = "/media/"
